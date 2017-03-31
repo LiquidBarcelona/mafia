a5ab639d9f999a88b4d20c31d0895f046c438490
Marc Bernet <mbernet@gmail.com>
2017-03-31 13:28:31 +0200

    init code test mafia

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'mafia');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'password');
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+require 'config/config.php';
+require 'vendor/autoload.php';
+$app = new Slim\App();
+$container = $app->getContainer();
+$container['view'] = function ($container) {
+	$view = new \Slim\Views\Twig('views', [
+		'cache' => false
+	]);
+	$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+	$view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+	return $view;
+};
+$dbh = new \PDO("mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD);
+
+$app->get('/members', function ($request, $response, $args) use ($app, $dbh) {
+
+	$sql = "SELECT members.*, capo.name as capo_name
+			FROM members LEFT JOIN members as capo
+			ON members.capo_id = capo.id ORDER BY years DESC";
+
+	$rs = $dbh->query($sql, PDO::FETCH_ASSOC);
+	$members = $rs->fetchAll(PDO::FETCH_OBJ);
+
+	return $this->view->render($response, 'members.html', [
+		'members' => $members
+	]);
+});
+
+$app->get('/imprison/{id}', function ($request, $response, $args) use ($app, $dbh) {
+	$response->write('Mètode empresonar');
+});
+
+
+$app->get('/compare', function ($request, $response, $args) use ($app, $dbh) {
+	return $this->view->render($response, 'compare.html');
+});
+
+$app->post('/compare', function ($request, $response, $args) use ($app, $dbh) {
+	$response->write('Algoritme de comparar');
+});
+
+$app->run();
